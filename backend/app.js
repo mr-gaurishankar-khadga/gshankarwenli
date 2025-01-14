932960bcaecff1bd313f653bccbaa29e39e0664e
uday871 <uday871@gmail.com>
2025-01-14 14:10:01 +0530

    port added

--- backend/app.js
+++ backend/app.js
@@ -94,9 +94,9 @@ app.get('/test', (req, res) => {
 
 
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
