e2850043fb81373def4817b104b4eea5c6d3bd70
uday871 <uday871@gmail.com>
2025-01-14 15:03:58 +0530

    mrx to

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ const reviewRoutes = require('./routes/reviewRoutes');
 const app = express();
 
 app.use(cors({
-  origin: 'http://www.wenli.in',
+  origin: 'https://www.wenli.in',
 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
