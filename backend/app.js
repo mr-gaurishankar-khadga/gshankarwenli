c39051ecb4e16609cd8833c116e7ec31d73bd209
MR GAURI SHANKAR KHADGA <udaymaharshi07@gmail.com>
2025-01-22 07:11:35 +0530

    Update app.js
    nothing

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const app = express();
 
 
 const corsOptions = {
-  origin: 'https://wenli.netlify.app/',
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
